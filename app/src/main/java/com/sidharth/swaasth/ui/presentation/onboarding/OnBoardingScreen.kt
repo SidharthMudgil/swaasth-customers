4e826cdcbb4a4cc5906d1ae94c1a0d43766fe8ee
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-02-29 18:29:47 +0530

    implement OnBoardingScreen

---
+++ app/src/main/java/com/sidharth/swaasth/ui/presentation/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,41 @@
+package com.sidharth.swaasth.ui.presentation.onboarding
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun OnBoardingScreen() {
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(model = "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png", contentDescription = null)
+        AsyncImage(model = "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png", contentDescription = null)
+        Text(text = "Introducing Swaasth")
+        Text(text = "With Swaasth, accessing healthcare services is effortless and efficient. Take control of your health journey with the convenience of Swaasth at your fingertips.")
+        Card(
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Let's Start")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun OnBoardingScreenPreview() {
+    OnBoardingScreen()
+}
