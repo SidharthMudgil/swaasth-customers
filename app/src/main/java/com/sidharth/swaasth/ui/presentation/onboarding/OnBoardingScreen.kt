97cfc177faafc26ae2a3edb3138c486ecf428cdb
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-03-02 18:30:49 +0530

    update OnBoardingScreen ui

--- app/src/main/java/com/sidharth/swaasth/ui/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/sidharth/swaasth/ui/presentation/onboarding/OnBoardingScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -18,23 +20,40 @@ import coil.compose.AsyncImage
 fun OnBoardingScreen() {
     Column(
         modifier = Modifier.fillMaxHeight(),
-        verticalArrangement = Arrangement.SpaceEvenly,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(model = "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png", contentDescription = null)
-        AsyncImage(model = "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png", contentDescription = null)
+        AsyncImage(
+            model = "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png",
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+        )
+        AsyncImage(
+            model = "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png",
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
         Text(text = "Introducing Swaasth")
         Text(text = "With Swaasth, accessing healthcare services is effortless and efficient. Take control of your health journey with the convenience of Swaasth at your fingertips.")
         Card(
             shape = RoundedCornerShape(8.dp),
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Let's Start")
+            Text(
+                text = "Let's Start",
+                Modifier
+                    .padding(16.dp, 8.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun OnBoardingScreenPreview() {
     OnBoardingScreen()
