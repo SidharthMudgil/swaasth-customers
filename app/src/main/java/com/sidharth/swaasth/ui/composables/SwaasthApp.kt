b8896c15b50093b5a6c89c28b63f9a5d0a2ec7f9
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-03-17 01:27:36 +0530

    add bottom nav bar

--- app/src/main/java/com/sidharth/swaasth/ui/composables/SwaasthApp.kt
+++ app/src/main/java/com/sidharth/swaasth/ui/composables/SwaasthApp.kt
@@ -1,10 +1,18 @@
 package com.sidharth.swaasth.ui.composables
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.sidharth.swaasth.ui.theme.SwaasthTheme
 
@@ -24,7 +32,18 @@ fun SwaasthApp(finishActivity: () -> Unit) {
 
 @Composable
 fun BottomNavigationBar() {
-    NavigationBar {
+    val items = listOf("Songs", "Artists", "Playlists")
+    var selectedItem by rememberSaveable { mutableIntStateOf(0) }
 
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                alwaysShowLabel = true,
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
     }
 }
