c17c3299887229bcf80771aef912840cf39d8f2d
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-03-17 01:27:36 +0530

    add AppConstants

---
+++ app/src/main/java/com/sidharth/swaasth/common/constant/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.sidharth.swaasth.common.constant
+
+object AppConstants {
+    const val HOME = "Home"
+    const val PROFILE = "Profile"
+    const val APPOINTMENTS = "Appointments"
+    const val LIVE_QUEUE = "Queue"
+}
