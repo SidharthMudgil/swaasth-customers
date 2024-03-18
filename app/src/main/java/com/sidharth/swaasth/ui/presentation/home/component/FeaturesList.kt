12b70548fa4bf211b1ef002029132397327c3d8b
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-03-18 23:11:35 +0530

    implement FeaturesList

--- app/src/main/java/com/sidharth/swaasth/ui/presentation/home/component/FeaturesList.kt
+++ app/src/main/java/com/sidharth/swaasth/ui/presentation/home/component/FeaturesList.kt
@@ -1,10 +1,46 @@
 package com.sidharth.swaasth.ui.presentation.home.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
 
 @Composable
 fun FeaturesList() {
+    val features = listOf("")
+
+    Column {
+        Row {
+            Text(text = "Specialities")
+            Text(text = "See all")
+        }
+
+        LazyRow {
+            items(features) { item ->
+                SpecialityCard(item)
+            }
+        }
+    }
+}
+
+@Composable
+fun SpecialityCard(
+    speciality: String
+) {
+    Column {
+        AsyncImage(model = "", contentDescription = null)
+        Text(text = speciality)
+    }
+}
+
+@Preview
+@Composable
+private fun SpecialityCardPreview() {
+    SpecialityCard("General Physician")
 }
 
 
