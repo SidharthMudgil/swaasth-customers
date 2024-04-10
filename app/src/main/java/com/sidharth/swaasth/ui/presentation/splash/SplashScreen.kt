596f29978bb9a1056d73fae50bc8b2717fe26ebd
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-04-11 01:07:36 +0530

    complete ui for SplashScreen

--- app/src/main/java/com/sidharth/swaasth/ui/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/sidharth/swaasth/ui/presentation/splash/SplashScreen.kt
@@ -1,25 +1,19 @@
 package com.sidharth.swaasth.ui.presentation.splash
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
 @Composable
 fun SplashScreen() {
     Scaffold {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
@@ -30,21 +24,7 @@ fun SplashScreen() {
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp)
-            )
-            AsyncImage(
-                model = "",
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-            )
-            AsyncImage(
-                model = "",
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
+                    .fillMaxHeight()
             )
         }
     }
