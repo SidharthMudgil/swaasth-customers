52fda0e21bfcf78290820e464f3b4020e7dbc6df
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-04-14 16:46:54 +0530

    style: add images for LoginScreen & OnBoardingScreen

--- app/src/main/java/com/sidharth/swaasth/ui/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/sidharth/swaasth/ui/presentation/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.sidharth.swaasth.ui.presentation.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.ClickableText
@@ -38,13 +40,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import com.sidharth.swaasth.R
 import com.sidharth.swaasth.ui.component.InputField
 import com.sidharth.swaasth.ui.theme.Grey10
 import com.sidharth.swaasth.ui.theme.Grey20
@@ -67,12 +71,12 @@ fun LoginScreen(onClick: () -> Unit) {
                 .padding(16.dp)
                 .verticalScroll(rememberScrollState())
         ) {
-            AsyncImage(
-                model = "",
+            Image(
+                painter = painterResource(id = R.drawable.doctor),
                 contentDescription = null,
                 modifier = Modifier
                     .height(300.dp)
-                    .background(Grey10)
+                    .fillMaxWidth()
             )
 
             Spacer(modifier = Modifier.padding(top = 30.dp))
@@ -219,18 +223,22 @@ fun LoginScreen(onClick: () -> Unit) {
             }
 
             Spacer(modifier = Modifier.padding(top = 16.dp))
-            AsyncImage(
-                model = "",
+            Image(
+                painter = painterResource(id = R.drawable.google),
                 contentDescription = null,
                 modifier = Modifier
-                    .size(60.dp)
-                    .background(Grey10)
+                    .border(1.dp, Grey10, CircleShape)
+                    .size(48.dp)
             )
 
             Spacer(modifier = Modifier.padding(top = 16.dp))
-            Text(text = "By continuing, you are agree to our")
+            Text(
+                text = "By continuing, you are agree to our",
+                fontSize = 14.sp
+            )
             Row(
-                horizontalArrangement = Arrangement.spacedBy(6.dp)
+                horizontalArrangement = Arrangement.spacedBy(6.dp),
+                modifier = Modifier.padding(top = 4.dp)
             ) {
                 ClickableText(
                     text = buildAnnotatedString {
@@ -238,7 +246,11 @@ fun LoginScreen(onClick: () -> Unit) {
                     },
                     onClick = {
 
-                    }
+                    },
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        color = Grey20
+                    )
                 )
 
                 ClickableText(
@@ -247,7 +259,11 @@ fun LoginScreen(onClick: () -> Unit) {
                     },
                     onClick = {
 
-                    }
+                    },
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        color = Grey20
+                    )
                 )
 
                 ClickableText(
@@ -256,7 +272,11 @@ fun LoginScreen(onClick: () -> Unit) {
                     },
                     onClick = {
 
-                    }
+                    },
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        color = Grey20
+                    )
                 )
             }
         }
--- app/src/main/java/com/sidharth/swaasth/ui/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/sidharth/swaasth/ui/presentation/onboarding/OnBoardingScreen.kt
@@ -1,6 +1,6 @@
 package com.sidharth.swaasth.ui.presentation.onboarding
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,13 +15,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import com.sidharth.swaasth.R
 import com.sidharth.swaasth.ui.theme.Grey80
 
 @Composable
@@ -33,23 +34,23 @@ fun OnBoardingScreen(onClick: () -> Unit) {
             .fillMaxHeight()
             .padding(16.dp),
     ) {
-        AsyncImage(
-            model = "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png",
+        Image(
+            painter = painterResource(id = R.drawable.logo_hor),
             contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(80.dp)
-                .background(Color.LightGray)
         )
 
         Spacer(modifier = Modifier.padding(bottom = 32.dp))
-        AsyncImage(
-            model = "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png",
+        Image(
+            painter = painterResource(id = R.drawable.joinus),
             contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(1.3f)
-                .background(Color.LightGray)
         )
 
         Spacer(modifier = Modifier.padding(bottom = 32.dp))
